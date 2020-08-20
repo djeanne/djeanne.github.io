ea0105f103a90bab0413f90d83008c9c0adb0265
Joanna Shevchuk <joshev@protonmail.com>
2020-08-21 00:13:51 +0300

    Updates

---
+++ precache-manifest.ccea4326ed008a58f3c4b600aec81250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e1a4a422aff863dd8a32da97d805d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01bf0715d410c0005ad",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "236f392f7cd88724c54a",
+    "url": "/static/css/main.f9e3018f.chunk.css"
+  },
+  {
+    "revision": "e01bf0715d410c0005ad",
+    "url": "/static/js/2.67b6326f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.67b6326f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236f392f7cd88724c54a",
+    "url": "/static/js/main.2c00ff5f.chunk.js"
+  },
+  {
+    "revision": "0cbf109810e1f4174462",
+    "url": "/static/js/runtime-main.d13e52b3.js"
+  }
+]);
